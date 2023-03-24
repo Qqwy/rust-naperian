e4221d1aa08dc54cd3d88645470f7995f0969a5c
qqwy <qqwy@gmx.com>
2023-03-24 12:39:06 +0100

    Some more operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use sealed::sealed;
 use std::marker::PhantomData;
 
+#[sealed]
 pub trait TVec<T>: Sized {
     const LEN: usize;
     fn len(&self) -> usize {
@@ -14,6 +15,8 @@ pub trait TVec<T>: Sized {
         }
     }
 
+    fn replicate<F: Fn() -> T>(elem_fun: F) -> Self;
+
     // fn vmap<U, Us: TVec<U>>(self, fun: impl Fn(T) -> U) -> Us;
 }
 
@@ -21,11 +24,14 @@ pub trait TVec<T>: Sized {
 #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
 pub struct TVecNil;
 
+#[sealed]
 impl<T> TVec<T> for TVecNil {
     const LEN: usize = 0;
 
+    fn replicate<F: Fn() -> T>(_elem_fun: F) -> Self {
+        TVecNil
+    }
 }
-
 
 #[derive(PartialEq, Debug, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
 pub struct TVecCons<T, Tail>{
@@ -33,15 +39,25 @@ pub struct TVecCons<T, Tail>{
     pub tail: Tail,
 }
 
+#[sealed]
 impl<T, Tail: TVec<T>> TVec<T> for TVecCons<T, Tail> {
     const LEN : usize = 1 + Tail::LEN;
+
+    fn replicate<F: Fn() -> T>(elem_fun: F) -> Self {
+        TVecCons {
+            head: elem_fun(),
+            tail: Tail::replicate(elem_fun)
+        }
+    }
 }
 
+#[sealed]
 pub trait TVecMappable<Mapper> {
     type Output;
     fn map(self, mapper: Mapper) -> Self::Output;
 }
 
+#[sealed]
 impl<F> TVecMappable<F> for TVecNil {
     type Output = TVecNil;
 
@@ -50,6 +66,7 @@ impl<F> TVecMappable<F> for TVecNil {
     }
 }
 
+#[sealed]
 impl<F, R, T, Tail> TVecMappable<F> for TVecCons<T, Tail>
 where
     F: Fn(T) -> R,
@@ -61,6 +78,35 @@ where
         TVecCons {
             head: fun(head),
             tail: tail.map(fun),
+        }
+    }
+}
+
+#[sealed]
+pub trait TVecZippable<Other> {
+    type Zipped;
+
+    fn zip(self, other: Other) -> Self::Zipped;
+}
+
+#[sealed]
+impl TVecZippable<TVecNil> for TVecNil {
+    type Zipped = TVecNil;
+    fn zip(self, _other: TVecNil) -> Self::Zipped {
+        TVecNil
+    }
+}
+
+#[sealed]
+impl<T, T2, Tail, Tail2> TVecZippable<TVecCons<T2, Tail2>> for TVecCons<T, Tail>
+where
+    Tail: TVecZippable<Tail2>
+{
+    type Zipped = TVecCons<(T, T2), Tail::Zipped>;
+    fn zip(self, other: TVecCons<T2, Tail2>) -> Self::Zipped {
+        TVecCons {
+            head: (self.head, other.head),
+            tail: self.tail.zip(other.tail)
         }
     }
 }

6c8e6167f046b7a9c1e1b8aed83a9226277ec760
qqwy <qqwy@gmx.com>
2023-03-24 13:00:55 +0100

    implement zipWith

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(type_name_of_val)]
 use sealed::sealed;
 use std::marker::PhantomData;
 
@@ -115,6 +116,80 @@ where
     }
 }
 
+#[sealed]
+pub trait TVecZipWith<Other, F>: TVecZippable<Other> {
+    type Output;
+    fn zip_with(self, other: Other, fun: F) -> Self::Output;
+}
+
+#[sealed]
+impl<F> TVecZipWith<TVecNil, F> for TVecNil {
+    type Output = TVecNil;
+    fn zip_with(self, _other: TVecNil, _fun: F) -> Self::Output {
+        TVecNil
+    }
+}
+
+
+#[sealed]
+impl<T, T2, Tail, Tail2, F, R> TVecZipWith<TVecCons<T2, Tail2>, F> for TVecCons<T, Tail>
+    where
+    F: Fn(T, T2) -> R,
+    Tail: TVecZipWith<Tail2, F>,
+{
+    type Output = TVecCons<R, <Tail as TVecZipWith<Tail2, F>>::Output>;
+    fn zip_with(self, other: TVecCons<T2, Tail2>, fun: F) -> Self::Output {
+        TVecCons {
+            head: fun(self.head, other.head),
+            tail: self.tail.zip_with(other.tail, fun),
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let v123 = TVecNil.prepend(3).prepend(2).prepend(1);
+        println!("{:?}", std::any::type_name_of_val(&v123));
+        println!("{:?}", v123);
+        println!("{:?}", v123.len());
+    }
+
+    #[test]
+    fn example2() {
+        let v123 = TVecNil.prepend(3).prepend(2).prepend(1);
+
+        let v456 = v123.map(|x| x + 3);
+        println!("{:?}", std::any::type_name_of_val(&v456));
+        println!("{:?}", v456);
+        println!("{:?}", v456.len());
+    }
+
+    #[test]
+    fn lift_square() {
+        let v123 = TVecNil.prepend(3).prepend(2).prepend(1);
+        let result = v123.zip(TVecReplicate::replicate(3));
+
+        println!("{:?}", std::any::type_name_of_val(&result));
+        println!("{:?}", result);
+        println!("{:?}", result.len());
+
+        let result2 = result.map(|(x, y)| x + y);
+
+        println!("{:?}", std::any::type_name_of_val(&result2));
+        println!("{:?}", result2);
+        println!("{:?}", result2.len());
+
+        let result3 = v123.zip_with(TVecReplicate::replicate(3), std::ops::Add::add);
+        println!("{:?}", std::any::type_name_of_val(&result3));
+        println!("{:?}", result3);
+        println!("{:?}", result3.len());
+    }
+}
+
 // /// Peano arithmetic
 // #[sealed]
 // pub trait Nat {
@@ -179,17 +254,17 @@ where
 
 
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }

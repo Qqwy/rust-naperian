e69ddefa5ea3863144451a9013d844ebaadf64dd
qqwy <qqwy@gmx.com>
2023-03-27 14:48:15 +0200

    Newer, nicer, Naperian trait

--- src/v2.rs
+++ src/v2.rs
@@ -216,8 +216,8 @@ impl<A, B, F: Fn(&A) -> B, N: ArrayLength> Apply<A, B, F> for GenericArray<F, N>
 /// Indeed, [`Mappable2::map2`] is the same as the `liftA2` operation you might know from Haskell's Applicative typeclass.
 ///
 /// In other words,
-/// Mappable2 can always be implemented for containers implementing [`Apply`] and [`New`],
-/// by currying `fun` and leveraging [`New::new`] and [`Apply::ap`]:
+/// Mappable2 can always be implemented for containers implementing [`Apply`] and [`New`] (or [`NewFrom`]),
+/// by currying `fun` and leveraging [`New::new`] ([`NewFrom::new_from`]) and [`Apply::ap`]:
 ///
 /// ```rust
 /// impl<A: Clone, U> Mappable2<A, U> for Option<A> {
@@ -311,6 +311,185 @@ impl<A, U, N: ArrayLength> Mappable3<A, U> for GenericArray<A, N> {
             let two = &second[pos];
             let three = &third[pos];
             fun(one, two, three)
+        })
+    }
+}
+
+pub trait Naperian<T>: Container {
+    type Log;
+    fn lookup(&self, index: Self::Log) -> &T;
+    /// Follows the paper more closely.
+    /// Unfortunately,
+    /// currently requires boxing the returned function.
+    ///
+    /// In the future, this might be improved
+    /// (Once the feature `#![feature(return_position_impl_trait_in_trait)]` is stabilized,
+    /// we could write this as `fn lookup_<'a>(&'a self) -> impl Fn(Self::Log) -> &'a T;`.)
+    fn lookup_<'a>(&'a self) -> Box<dyn Fn(Self::Log) -> &'a T + 'a>;
+    fn tabulate(fun: impl Fn(Self::Log) -> T) -> Self;
+    fn positions() -> Self::Containing<Self::Log>;
+}
+
+
+pub trait IsTrue {}
+impl IsTrue for B1 {}
+
+#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+pub struct Fin<N: Unsigned> {
+    val: usize,
+    _phantom: core::marker::PhantomData<N>,
+}
+
+impl<N: Unsigned> core::fmt::Debug for Fin<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = format!("Fin<U{}>", N::USIZE);
+        f.debug_tuple(&name)
+            .field(&self.val)
+            .finish()
+    }
+}
+
+
+impl<N: Unsigned> Fin<N> {
+    /// Creates a new Fin from a usize. Fallible, checked at runtime.
+    ///
+    /// A BoundError is returned when the usize is larger than the maximum bound (`N`).
+    #[inline(always)]
+    pub const fn new(val: usize) -> Result<Self, BoundError<N>> {
+        if val < N::USIZE {
+            Ok(Fin{val, _phantom: core::marker::PhantomData })
+        } else {
+            Err(BoundError::<N>::new())
+        }
+    }
+
+    /// Creates a new Fin from a usize.
+    ///
+    /// # Safety
+    /// The caller is responsible for making sure that `val` is smaller than `N::USIZE`.
+    pub unsafe fn new_unchecked(val: usize) -> Self {
+        Fin{val, _phantom: core::marker::PhantomData}
+    }
+
+    /// Creates a new Fin from an unsigned typenum.
+    ///
+    /// Outcome is always valid as too large values result in a compile error.
+    #[inline(always)]
+    pub const fn tnew<Val>() -> Self
+        where
+        Val: Unsigned + typenum::IsLess<N>,
+        typenum::Le<Val, N>: IsTrue,
+    {
+        Fin{val: Val::USIZE, _phantom: core::marker::PhantomData}
+    }
+
+    /// Creates a new Fin from an compile-time constant usize.
+    ///
+    /// Outcome is always valid as too large values result in a compile error.
+    #[inline(always)]
+    pub const fn cnew<const VAL: usize>() -> Self
+        where
+        typenum::Const<VAL>: typenum::ToUInt,
+        typenum::U<VAL>: Unsigned + typenum::IsLess<N>,
+        typenum::Le<typenum::U<VAL>, N>: IsTrue,
+    {
+        Fin{val: VAL, _phantom: core::marker::PhantomData}
+    }
+
+    #[inline(always)]
+    pub const fn bound() -> usize {
+        N::USIZE
+    }
+}
+
+#[derive(Debug)]
+pub struct BoundError<N: Unsigned> {
+    _phantom: core::marker::PhantomData<N>,
+}
+
+impl<N: Unsigned> BoundError<N> {
+    pub const fn new() -> Self {
+        Self {_phantom: core::marker::PhantomData}
+    }
+}
+
+impl<N: Unsigned> TryInto<Fin<N>> for usize {
+    type Error = BoundError<N>;
+    fn try_into(self) -> Result<Fin<N>, Self::Error> {
+        Fin::<N>::new(self)
+    }
+}
+
+pub trait UnsignedExt: Unsigned {
+    #[inline(always)]
+    fn fin<N: Unsigned>() -> Fin<N>
+    where
+        Self: Unsigned + typenum::IsLess<N>,
+        typenum::Le<Self, N>: IsTrue,
+    {
+        Fin::<N>::tnew::<Self>()
+    }
+}
+
+impl<Index: Unsigned> UnsignedExt for Index {}
+
+pub fn fin<const VAL: usize, N: Unsigned>() -> Fin<N>
+where
+    typenum::Const<VAL>: typenum::ToUInt,
+    typenum::U<VAL>: Unsigned + typenum::IsLess<N>,
+    typenum::Le<typenum::U<VAL>, N>: IsTrue,
+{
+    Fin::<N>::cnew::<VAL>()
+}
+
+
+
+impl<T> Naperian<T> for Pair<T> {
+    type Log = bool;
+    fn lookup(&self, index: Self::Log) -> &T {
+        match index {
+            false => &self.0,
+            true => &self.1,
+        }
+    }
+    fn lookup_<'a>(&'a self) -> Box<dyn Fn(Self::Log) -> &'a T + 'a> {
+        Box::new(|index| {
+            match index {
+                false => &self.0,
+                true => &self.1,
+            }
+        })
+    }
+    fn tabulate(fun: impl Fn(Self::Log) -> T) -> Self {
+        Pair(fun(false), fun(true))
+    }
+
+    fn positions() -> Self::Containing<Self::Log> {
+        Pair(false, true)
+    }
+}
+
+impl<T, N: ArrayLength> Naperian<T> for GenericArray<T, N> {
+    type Log = Fin<N>;
+    fn lookup(&self, index: Self::Log) -> &T {
+        &self[index.val]
+    }
+    fn lookup_<'a>(&'a self) -> Box<dyn Fn(Self::Log) -> &'a T + 'a> {
+        Box::new(|index| {
+            &self[index.val]
+        })
+    }
+    fn positions() -> Self::Containing<Self::Log> {
+        GenericArray::generate(|pos| {
+            // SAFETY: pos is in range [0..N)
+            unsafe { Fin::new_unchecked(pos) }
+        })
+    }
+    fn tabulate(fun: impl Fn(Self::Log) -> T) -> Self {
+        GenericArray::generate(|pos| {
+            // SAFETY: pos is in range [0..N)
+            let fin = unsafe { Fin::new_unchecked(pos) };
+            fun(fin)
         })
     }
 }

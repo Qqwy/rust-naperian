c71db6d1d1c394c8f305f8bb8fd8d254f6a8a914
qqwy <qqwy@gmx.com>
2023-03-29 20:12:22 +0200

    Extra type aliases

--- src/aliases.rs
+++ src/aliases.rs
@@ -1,5 +1,10 @@
 
-use super::{Array, HCons, HNil, Prism, Scalar};
+use super::{Array, HCons, HNil, Prism};
+
+/// A single scalar value.
+///
+/// This is a rank-0 tensor.
+pub type Scalar<T> = super::Scalar<T>;
 
 /// Vect, a Vector with a statically-known size.
 ///
@@ -24,3 +29,11 @@ pub type Mat<T, Rows, Cols> = Prism<T, Vect<Array<T, Cols>, Rows>, Cols, HCons<R
 /// only that it implements the [`Hyper`] trait which contains many common operations.
 pub type Tensor3<T, Slices, Rows, Cols> =
     Prism<T, Mat<Array<T, Cols>, Slices, Rows>, Cols, HCons<Rows, HCons<Slices, HNil>>>;
+
+/// Rank-4 tensors (blocks, slices, rows, columns).
+///
+/// This is a type alias.
+/// During normal usage you do not need to understand the backing type,
+/// only that it implements the [`super::Hyper`] trait which contains many common operations.
+pub type Tensor4<T, Blocks, Slices, Rows, Cols> =
+    Prism<T, Tensor3<Array<T, Cols>, Blocks, Slices, Rows>, Cols, HCons<Rows, HCons<Slices, HCons<Blocks, HNil>>>>;
--- src/const_aliases.rs
+++ src/const_aliases.rs
@@ -1,6 +1,11 @@
 
-use super::{Array, HCons, HNil, Prism, Scalar};
+use super::{Array, HCons, HNil, Prism};
 use typenum::U;
+
+/// A single scalar value.
+///
+/// This is a rank-0 tensor.
+pub type Scalar<T> = super::Scalar<T>;
 
 /// Vect, a Vector with a statically-known size.
 ///
@@ -19,10 +24,18 @@ pub type Vect<T, const N: usize> = Prism<T, Scalar<Array<T, U<N>>>, U<N>, HNil>;
 pub type Mat<T, const ROWS: usize, const COLS: usize> =
     Prism<T, Vect<Array<T, U<COLS>>, ROWS>, U<COLS>, HCons<U<ROWS>, HNil>>;
 
-/// Vect, a Vector with a statically-known size.
+/// Rank-3 tensors (slices, rows, columns).
 ///
 /// This is a type alias.
 /// During normal usage you do not need to understand the backing type,
 /// only that it implements the [`super::Hyper`] trait which contains many common operations.
 pub type Tensor3<T, const SLICES: usize, const ROWS: usize, const COLS: usize> =
     Prism<T, Mat<Array<T, U<COLS>>, SLICES, ROWS>, U<COLS>, HCons<U<ROWS>, HCons<U<SLICES>, HNil>>>;
+
+/// Rank-4 tensors (blocks, slices, rows, columns).
+///
+/// This is a type alias.
+/// During normal usage you do not need to understand the backing type,
+/// only that it implements the [`super::Hyper`] trait which contains many common operations.
+pub type Tensor4<T, const BLOCKS: usize, const SLICES: usize, const ROWS: usize, const COLS: usize> =
+    Prism<T, Tensor3<Array<T, U<COLS>>, BLOCKS, SLICES, ROWS>, U<COLS>, HCons<U<ROWS>, HCons<U<SLICES>, HCons<U<BLOCKS>, HNil>>>>;
